8e72683ab2ef2eff8948272a62d35fae4215855b
Francisco Lopez <franlopbri@gmail.com>
2020-05-20 14:19:34 -0500

    se aliminan documentos externos

--- Siseac/creaSesion.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-session_start();
-
-$_SESSION['account_name']='gcanol';
-$_SESSION['tipo_usuario']=3;
-$_SESSION['estatus']=1;
- ?>
--- Siseac/profesor/include/leftmenu.php
+++
@@ -1,8 +0,0 @@
-  <div id="left_menu">
-                    <ul id="main_menu" class="main_menu">
-<!--                      <li class="select" ><a href="#"style="color: #292929;" ><b>Dashboard</b></a></li>-->
-                    <li class="select"  ><a href="profile.php" style="color: #292929;"> Entrevista Inicial </a></li>
-    
-                     
-                    </ul>
-               </div>
